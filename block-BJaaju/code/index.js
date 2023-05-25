3501f4807802261629003cc9f2478417f80ac6b9
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-05-25 13:42:39 +0530

    Unsplash Image Search

---
+++ block-BJaaju/code/index.js
@@ -0,0 +1,18 @@
+let root = document.querySelector(".root");
+const accessKey = CXhethHMEnhUcr5YqFZ7pYjBzhoPNcHu5bDtg-vEHf8;
+
+let input = document.querySelector('input');
+
+function fetch(url, handleSuccess) {
+    let 
+}
+
+function handleInput(event) {
+    if(event.keyCode === 13 && input.value) {
+        let url = 
+
+        input.value = "";
+    }
+}
+
+input.addEventListener("keydown", handleInput);
